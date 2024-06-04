6e45df6f87cdf07053e499005c1dc6d23e3efcf8
TosakaUCW <tosakaucw0@gmail.com>
2024-06-04 15:06:26 +0800

    🎉 init(Assignment1): Q2 SVM Done

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -55,7 +55,27 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    for i in range(num_train):
+        scores = X[i].dot(W)
+        correct_class_score = scores[y[i]]
+        for j in range(num_classes):
+            if j == y[i]:
+                continue
+            margin = scores[j] - correct_class_score + 1  # note delta = 1
+            if margin > 0:
+                # loss += margin
+
+                # update gradient for incorrect label
+                dW[:, j] += X[i].T
+
+                # update gradient for correct label
+                dW[:, y[i]] -= X[i].T
+
+    # scale gradient ovr the number of samples
+    dW /= num_train
+
+    # append partial derivative of regularization term
+    dW += 2 * reg * W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -78,7 +98,12 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_train = X.shape[0]
+    scores = X @ W  # of shape (num_train, num_classes)
+    scores_correct = scores[range(num_train), y][:, np.newaxis]
+    margins = np.maximum(0, scores - scores_correct + 1)
+    margins[range(num_train), y] = 0
+    loss = margins.sum() / num_train + reg * np.sum(W**2)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -93,7 +118,12 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    dW = (margins > 0).astype(int)
+    
+    # update gradient to include correct labels
+    dW[range(num_train), y] -= dW.sum(axis=1)
+    
+    dW = X.T @ dW / num_train + 2 * reg * W   # gradient w.r.t. W
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
